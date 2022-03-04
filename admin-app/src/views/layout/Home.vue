6dedd973c338cae4c7568f0fc753c79ce8d3842e
messenger <269170236@qq.com>
2022-03-04 20:33:05 +0800

    左边栏

--- admin-app/src/views/layout/Home.vue
+++ admin-app/src/views/layout/Home.vue
@@ -1,6 +1,50 @@
 <template>
   <div class="home">
-    <div style="width: 256px">
+    <!-- 菜单列表 -->
+    <div class="menu-list">
+      <a-menu
+        :default-selected-keys="['1']"
+        :default-open-keys="['sub1']"
+        mode="inline"
+        theme="dark"
+        :inline-collapsed="collapsed"
+      >
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="inbox" />
+          <span>Option 3</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <span slot="title"
+            ><a-icon type="mail" /><span>Navigation One</span></span
+          >
+          <a-menu-item key="5"> Option 5 </a-menu-item>
+          <a-menu-item key="6"> Option 6 </a-menu-item>
+          <a-menu-item key="7"> Option 7 </a-menu-item>
+          <a-menu-item key="8"> Option 8 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"
+            ><a-icon type="appstore" /><span>Navigation Two</span></span
+          >
+          <a-menu-item key="9"> Option 9 </a-menu-item>
+          <a-menu-item key="10"> Option 10 </a-menu-item>
+          <a-sub-menu key="sub3" title="Submenu">
+            <a-menu-item key="11"> Option 11 </a-menu-item>
+            <a-menu-item key="12"> Option 12 </a-menu-item>
+          </a-sub-menu>
+        </a-sub-menu>
+      </a-menu>
+    </div>
+    <!-- 内容 -->
+    <div :class="{'main-app':true,'menu-unfold':collapsed}">
       <!-- 列表按钮 -->
       <a-button
         type="primary"
@@ -9,54 +53,9 @@
       >
         <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
       </a-button>
-      <!-- 菜单列表 -->
-      <div class="menu-list">
-        <a-menu
-          :default-selected-keys="['1']"
-          :default-open-keys="['sub1']"
-          mode="inline"
-          theme="dark"
-          :inline-collapsed="collapsed"
-        >
-          <a-menu-item key="1">
-            <a-icon type="pie-chart" />
-            <span>Option 1</span>
-          </a-menu-item>
-          <a-menu-item key="2">
-            <a-icon type="desktop" />
-            <span>Option 2</span>
-          </a-menu-item>
-          <a-menu-item key="3">
-            <a-icon type="inbox" />
-            <span>Option 3</span>
-          </a-menu-item>
-          <a-sub-menu key="sub1">
-            <span slot="title"
-              ><a-icon type="mail" /><span>Navigation One</span></span
-            >
-            <a-menu-item key="5"> Option 5 </a-menu-item>
-            <a-menu-item key="6"> Option 6 </a-menu-item>
-            <a-menu-item key="7"> Option 7 </a-menu-item>
-            <a-menu-item key="8"> Option 8 </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title"
-              ><a-icon type="appstore" /><span>Navigation Two</span></span
-            >
-            <a-menu-item key="9"> Option 9 </a-menu-item>
-            <a-menu-item key="10"> Option 10 </a-menu-item>
-            <a-sub-menu key="sub3" title="Submenu">
-              <a-menu-item key="11"> Option 11 </a-menu-item>
-              <a-menu-item key="12"> Option 12 </a-menu-item>
-            </a-sub-menu>
-          </a-sub-menu>
-        </a-menu>
-      </div>
-      <!-- 内容 -->
-      <div class="main">
-        wefsd
-      </div>
     </div>
+    <div class="breadcrumb">面包血</div>
+    <div class="user-info">用户信息</div>
   </div>
 </template>
 
@@ -74,3 +73,8 @@ export default {
   },
 };
 </script>
+<style lang="less">
+// @import url("../../assets/css/home.less");
+// 前面没有一个 ~  会报错！因为他不知道作为一个模块来加载的
+@import url("~@/assets/css/home.less");
+</style>
